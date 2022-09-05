8bbbd3d15db9f52a46c2720532bf4de1caee41f1
Daniel Silva <daniel24maio@gmail.com>
2022-09-05 18:36:46 -0300

    request personalizado deixado a parte.

--- controle-games/app/Http/Requests/GamesFormRequest.php
+++ controle-games/app/Http/Requests/GamesFormRequest.php
@@ -24,7 +24,8 @@ class GamesFormRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'nome' => ['required','min:2'],
+            'plataforma' => ['required']
         ];
     }
 }
